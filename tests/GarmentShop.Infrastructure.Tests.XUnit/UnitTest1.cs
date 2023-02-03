f2f8599a4590cc47aa76570fd125dd99a4a0ce2f
StrdL <stas20001234567@mail.ru>
2023-02-03 22:16:07 +0200

    Add project files.

---
+++ tests/GarmentShop.Infrastructure.Tests.XUnit/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace GarmentShop.Infrastructure.Tests.XUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/GarmentShop.Presentation.Tests.XUnit/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace GarmentShop.Presentation.Tests.XUnit
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
